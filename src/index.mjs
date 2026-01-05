8430f8b5e3f860908b0b6b4a7d1f4d33df0b6ae5
aaronr24468 <aaronr24468@gmail.com>
2026-01-05 16:38:06 -0600

    change origin

--- src/index.mjs
+++ src/index.mjs
@@ -19,7 +19,7 @@ const app = express();
 const server = createServer(app)
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://chatty-ff143.firebaseapp.com',
     credentials: true
 }));
 app.use(morgan('dev'));

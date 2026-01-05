3d4c479beb0107bc781e15628d2c57a35977f555
aaronr24468 <aaronr24468@gmail.com>
2026-01-05 15:49:31 -0600

    pool limit

--- src/models/models.mjs
+++ src/models/models.mjs
@@ -2,11 +2,16 @@ import mysql from 'mysql2/promise';
 import {config} from 'dotenv'
 config();
 
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
     host: `${process.env.HOST}`,
     user: `${process.env.USER}`,
     password: `${process.env.PASSWORD}`,
-    database: `${process.env.DB}`
+    database: `${process.env.DB}`,
+
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0,
+    connectTimeout: 10000
 });
 
 await connection.connect((err) =>{
